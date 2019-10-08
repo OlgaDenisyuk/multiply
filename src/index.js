f0e6f0971727cf01c22414742aa451350d12fa69
Olga Denisyuk <denisykolga@yandex.ru>
2019-10-08 12:59:37 +0300

    fix: add 1 more solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 module.exports = function multiply(first, second) {
     if( first.length < second.length )
     {
-        var tmp = second;
+        let temp = second;
         second = first;
-        first = tmp;
+        first = temp;
     }
-    var a1 = first.split( '' );
-    var a2 = second.split( '' );
-    var r = [];
-    var rlen = a1.length + a2.length; 
+    let ar1 = first.split( '' ), ar2 = second.split( '' ), r = [];
+    let rlen = ar1.length + ar2.length; 
     for( var k = 0; k < rlen; k++ )
         r[ k ] = 0;
-    for( var j = 0; j < a2.length; j++ ) 
-        for( var i = 0; i < a1.length; i++ )
-            r[ 1 + i + j ] += a1[ i ] * a2[ j ];   
+    for( var j = 0; j < ar2.length; j++ ) 
+        for( var i = 0; i < ar1.length; i++ )
+            r[ 1 + i + j ] += ar1[ i ] * ar2[ j ];   
 
     for( var k = rlen - 1; k > 0; k-- )
         if( r[ k ] >= 10 )
